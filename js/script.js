b1f3ad4b487989122ff05032a4079912c0dc489e
Aman Bhardwaj <abhardwaj744@gmail.com>
2022-09-16 16:52:18 +0530

    update

---
+++ js/script.js
@@ -0,0 +1,79 @@
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDrKaWB2KbZ9lB0au2cTm1SNq11gQLsl08",
+    authDomain: "quixa-article.firebaseapp.com",
+    databaseURL: "https://quixa-article-default-rtdb.firebaseio.com",
+    projectId: "quixa-article",
+    storageBucket: "quixa-article.appspot.com",
+    messagingSenderId: "943403261797",
+    appId: "1:943403261797:web:3d63f2bad99ca49b014b8c"
+  };
+
+
+ firebase.initializeApp(firebaseConfig);
+
+ var quixaArticlesData = firebase.database().ref("quixaArticles/pending");
+
+ document.getElementById('quixaArticles').addEventListener("submit", submitForm);
+
+ function submitForm(e){
+    e.preventDefault();
+
+    var category = getElementVal('category');
+    var title = getElementVal('title');
+    var story = getElementVal('story');
+    var source = getElementVal('source');
+    var tags = getElementVal('tags');
+    var status = getElementVal('status');
+    
+
+    console.log(category, title, story, source, tags ,status);
+    saveArticlesData(category, title, story, source, tags ,status);
+
+    document.getElementById('quixaArticles').reset();
+
+ }
+
+const saveArticlesData = (category, title, story, source, tags, status) => {
+
+    var newquixaArticles = quixaArticlesData.push();
+
+    newquixaArticles.set({
+        category :category,
+        title : title,
+        story : story,
+        source : source,
+        tags : tags,
+        status : status,
+   
+    });
+
+};
+
+
+
+ const getElementVal = (id) => {
+    return document.getElementById(id).value;
+ };
+
+ //get data
+
+quixaArticlesData.once('value', function(snapshot){
+  if(snapshot.exists()){
+      var content = '';
+      snapshot.forEach(function(data){
+          var val = data.val();
+          content +='<div class="card my-4" style="width: 100%;">';
+          content +='<div class="card-body">'
+          content += '<div class="">Category - ' + val.category + '</div>';
+          content += '<h5 class="card-title">' + val.title + '</h5>';
+          content += '<p class="card-text">' + val.story + '</p>';
+          content += '<p class="card-text">Source - ' + val.source + '</p>';
+          content += '<p class="card-text">Tags - ' + val.tags + '</p>';
+          content += '</div>';
+          content += '</div>';
+          content += '</div>';
+      });
+      $('#showQuixaArticles').append(content);
+  }
+});
